799db4ce1423bb1dbb7dd3ec98cb3bdc6999f4c5
DiepHan26 <DiepHanDev@gmail.com>
2021-04-10 16:31:31 -0500

    movies api working with MongoDB using genre embedded doc

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const home = require("./routes/home");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
 
 const mongoose = require("mongoose");
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use("/", home);
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
 
 const port = process.env.PORT || 3000;
 app.listen(port);
