655f53c058d6f1341e68e2b0f9d2e72193c9cc37
DiepHan26 <DiepHanDev@gmail.com>
2021-04-12 23:28:56 -0500

    authentication api w/ jwt

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require("config");
 const Joi = require("Joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const genres = require("./routes/genres");
@@ -5,6 +6,12 @@ const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
+const auth = require("./routes/auth");
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined");
+  process.exit(1);
+}
 
 const mongoose = require("mongoose");
 
@@ -25,6 +32,7 @@ app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port);
