19b9c35b8e2c6468e786bb109de6285057729e1a
DiepHan26 <DiepHanDev@gmail.com>
2021-04-12 23:11:56 -0500

    persist hashed user password

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const { User, validate } = require("../models/user");
 const express = require("express");
@@ -8,11 +9,12 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  console.log(user);
+
   if (user) return res.status(400).send("User already registered");
 
   user = new User(_.pick(req.body, ["name", "email", "password"]));
-
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
   return res.send(_.pick(user, ["_id", "name", "email"]));
