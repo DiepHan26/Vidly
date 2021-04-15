ab7c597d16751c03c05aedfde468f01734bc1032
DiepHan26 <DiepHanDev@gmail.com>
2021-04-15 08:43:12 -0500

    handling async errors

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("express-async-errors");
+const error = require("./middleware/error");
 const config = require("config");
 const Joi = require("Joi");
 Joi.objectId = require("joi-objectid")(Joi);
@@ -33,6 +35,8 @@ app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port);
